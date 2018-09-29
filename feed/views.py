c608bdbf2859278511653de922c2cfa62c41ddd5
alexHooper <alexmohack@gmail.com>
2018-09-29 18:06:50 +0530

    add home_view

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Feed
+
+def home_view(request):
+	objects = Feed.objects.all().order_by('-id')
+	context = {'objects': objects}
+	return render(request, 'home.html', context)
