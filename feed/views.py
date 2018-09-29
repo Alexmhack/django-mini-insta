fca5bf43dbc7b7e4a005ddee15d0e0733d0d86b6
alexHooper <alexmohack@gmail.com>
2018-09-29 18:14:43 +0530

    add func to auth private channel

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from pusher import Pusher
 
 from .models import Feed
@@ -16,3 +17,15 @@ def home_view(request):
 	objects = Feed.objects.all().order_by('-id')
 	context = {'objects': objects}
 	return render(request, 'home.html', context)
+
+
+# function to authenticate private channel
+def pusher_authentication(request):
+	channel = request.GET.get('channel_name', None)
+	socket_id = request.GET.get('socket_id', None)
+	auth = pusher.authenticate(
+		channnel=channel,
+		socket_id=socket_id
+	)
+
+	return JsonResponse(json.dumps(auth), safe=False)
