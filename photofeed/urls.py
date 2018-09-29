1efcf2887ebaa6a0c71f72ea2865f1b533d653e7
alexHooper <alexmohack@gmail.com>
2018-09-29 18:53:03 +0530

    add urls for views

--- photofeed/urls.py
+++ photofeed/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
+from feed.views import home_view, pusher_authentication, push_feed
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('', home_view, name='home'),
+    path('push-feed/', push_feed, name='push-feed'),
+    path('push-authentication/', pusher_authentication, name='push-authentication'),
 ]
