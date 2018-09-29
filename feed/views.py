3667e3ecac89ae08f8d1f9b7a0e3a63d3d17e355
alexHooper <alexmohack@gmail.com>
2018-09-29 18:13:51 +0530

    instantiate pusher in views

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from pusher import Pusher
 
 from .models import Feed
 
+# instantiate pusher
+pusher = Pusher(
+	app_id=config("PUSHER_APP_ID"),
+	key=config("PUSHER_APP_KEY"),
+	secret=config("PUSHER_APP_SECRET"),
+	cluster=config("PUSHER_APP_CLUSTER")
+)
+
+# Home page view with all feeds
 def home_view(request):
 	objects = Feed.objects.all().order_by('-id')
 	context = {'objects': objects}
